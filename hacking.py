de64fe65adf94142a6aa6ffb7619b9530525534c
Aditya pandi team LEGEND-LX <102133364+Legends-lxp@users.noreply.github.com>
2022-03-30 11:57:54 +0530

    Create hacking.py

---
+++ hacking.py
@@ -0,0 +1,37 @@
+from input java 
+from input php 
+from input linux 
+from input shell
+
+await : in(os.).get 
+    await : in("clone fun hd from input")/n"
+      in : ("from_input")n/m" 
+        sending send.semeege.had get.var
+        var input("userbot"/n")/{legendhacker}
+                  
+                
+                  
+                  await utt
+                  
+async def _(e):
+    global que
+    if ABUSE == "ON":
+        usage = "/dreplyraid "
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None)
+        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        await e.get_reply_message()
+        if len(e.text) > 11:
+            message = str(legend[0])
+            a = await e.client.get_entity(message)
+            g = a.id                  
+                  
+      ["LEGEND BAAP OP BAKI SARE LEGEND KI TOPI"
+       "LEGEND IS OP" 
+        await f = .split(string_session)=None,
+       link = @MR_INVISIBLE_OFFICIAL
+       f"buttonurl://t.me/LegendHacker_IIN)
+       }
+       
+       
+Print(Legend_IN")n"
